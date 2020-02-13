4bd22aafc67cb7715614047b56328755607060ed
mcoue <marius@couet.net>
2020-02-13 14:55:43 -0600

    modifier interface pour effectuer des tests

--- src/vue/Interface.java
+++ src/vue/Interface.java
@@ -1,10 +1,24 @@
 package vue;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class Interface {
+public class Interface extends JFrame{
+    private JPanel pan = new JPanel();
+    private JButton b = new JButton("test");
+
+    public Interface(){
+        this.setTitle("Labyrinthe");
+        this.setSize(400, 400);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.setLayout(new GridLayout(2,1));
+        this.getContentPane().add(new JButton("test1"));
+        this.getContentPane().add(new JButton("test2"));
+        this.setVisible(true);
+    }
+
     public static void main(String[] args){
-        JFrame fenetre = new JFrame();
-        fenetre.setVisible(true);
+            Interface i = new Interface();
     }
 }

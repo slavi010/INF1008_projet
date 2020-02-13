5adee8f8a3de6c501eb089d66a932ce9b3e84a1a
slavi <sviatoslav.besnard@u-psud.fr>
2020-02-13 13:34:17 -0500

    add Case Labyrinthe + Case

---
+++ src/Labyrinthe.java
@@ -0,0 +1,22 @@
+public class Labyrinthe {
+    private int largeur;
+    private int longueur;
+    private int poidMax;
+    private Case[][] map;
+
+    public Labyrinthe(int largeur, int longueur, int poidMax) {
+        this.largeur = largeur;
+        this.longueur = longueur;
+        this.poidMax = poidMax;
+
+        map = new Case[longueur][largeur];
+    }
+
+    public Case get(int x, int y){
+        if (x >= 0 && x < longueur &&
+                y >= 0 && y < largeur){
+            return map[x][y];
+        }
+        return null;
+    }
+}
